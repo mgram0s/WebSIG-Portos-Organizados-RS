2f3559e090fbeee1ac65fabade4165a8aa26f6be
mgram0s <martina.ramos@aluno.riogrande.ifrs.edu.br>
2025-01-01 23:15:16 -0300

    mapa dinamico portos

---
+++ layers/23Unidadesdeconservacao_40.js
@@ -0,0 +1,8 @@
+var json_23Unidadesdeconservacao_40 = {
+"type": "FeatureCollection",
+"name": "23Unidadesdeconservacao_40",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nome": "Refúgio de Vida Silvestre do Molhe Leste - REVIS do Molhe Leste", "Ins. Legal": "Lei Municipal n° 007 de 10 de maio de 1996", "Area (m²)": 241928.0, "Observacao": "Delimitação do polígono e área informados no PDZ em conformidade com a base georreferenciada da Secretaria do Meio Ambiente (SEMA). A base geo daquela Secretaria diverge da área declarada na Lei n° 007/1996 de 300.000,00 m² (30 ha)." }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -52.076999181505201, -32.179335755715286 ], [ -52.074166684374802, -32.188000005721392 ], [ -52.076834289754203, -32.188000558288991 ], [ -52.079667906685799, -32.179332730050994 ], [ -52.076999181505201, -32.179335755715286 ] ] ] ] } }
+]
+}
